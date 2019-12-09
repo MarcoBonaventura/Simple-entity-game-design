bfb1450bbf3f196717caaa7bf2d24b21667616b1
Marco <marco.bonaventura@outlook.com>
2019-12-09 08:18:37 +0100

    Update Entity.h

--- Entity.h
+++ Entity.h
@@ -59,34 +59,34 @@ enum eFlag
     ENTITY_FLAG_MAPONLY     = 3,
     ENTITY_FLAG_ENEMY_S     = 4,
     ENTITY_FLAG_ENEMY_M     = 5,
-	ENTITY_FLAG_ENEMY_M1	= 6,
-	ENTITY_FLAG_ENEMY_M2	= 7,
+    ENTITY_FLAG_ENEMY_M1    = 6,
+    ENTITY_FLAG_ENEMY_M2    = 7,
     ENTITY_FLAG_ENEMY_F     = 8,
     ENTITY_FLAG_PLAYER      = 9,
     ENTITY_FLAG_BOSS        = 10,
     ENTITY_FLAG_GAMMARAY    = 11,
-    ENTITY_FLAG_MyBULLET	= 12
+    ENTITY_FLAG_MyBULLET    = 12
 };
 
 // how to move the entity
 enum eMove
 {
-	BEZIER 		= 0,
-	LOGARITMIC 	= 1,
-	BEZIER_BOSS = 2,
-	MOVE_DX 	= 3,
-	MOVE_SX 	= 4,
-	MOVE_DOWN 	= 5,
-	ESCAPE_DX 	= 6,
-	ESCAPE_SX 	= 7,
-	ROTATE 		= 8,
-	FALLING 	= 9,
-	SNAKE 		= 10,
-	PYRAMID     = 11,
-	DIAMOND     = 12,
-	GRID        = 13,
-    ASTEROID    = 14,
-    NONE        = 15
+    BEZIER 		= 0,
+    LOGARITMIC 		= 1,
+    BEZIER_BOSS     	= 2,
+    MOVE_DX 		= 3,
+    MOVE_SX 		= 4,
+    MOVE_DOWN 		= 5,
+    ESCAPE_DX 		= 6,
+    ESCAPE_SX 		= 7,
+    ROTATE 		= 8,
+    FALLING 		= 9,
+    SNAKE 		= 10,
+    PYRAMID         	= 11,
+    DIAMOND         	= 12,
+    GRID            	= 13,
+    ASTEROID       	= 14,
+    NONE            	= 15
 };
 
 // entity enemy fire mode
@@ -102,9 +102,9 @@ enum EnemyFireMode
 enum MedalFire
 {
     MEDAL_NONE      = 0,
-    MEDAL_TRIPLE 	= 1,
-    MEDAL_PENTA		= 2,
-    MEDAL_ROCKET	= 3,
+    MEDAL_TRIPLE    = 1,
+    MEDAL_PENTA	    = 2,
+    MEDAL_ROCKET    = 3,
     MEDAL_GAMMARAY  = 4,
     MEDAL_COMBOTOT  = 5
 
@@ -148,20 +148,16 @@ class Entity {
         SDL_Window*     Window;		
         SDL_Surface*    PrimarySurface;
 		
-		float       SpeedX;
-        float       SpeedY;
-		
-		float       AccelX;
-        float       AccelY;
-		float		kAcc;
-
+	float       SpeedX, SpeedY;
+        float       AccelX, AccelY;
+        float	    kAcc;
 
 
     public:
 	
-		static std::vector<CEntity*>EntityList;			// the main container of every entity
-		static std::map<int, CEntity*> EntityMap;		// 
-		static std::map<int, CEntity*> TargetEnemyMap;
+	static std::vector<CEntity*>EntityList;			// the main container of every entity
+	static std::map<int, CEntity*> EntityMap;		// 
+	static std::map<int, CEntity*> TargetEnemyMap;
 
         //static const int DOT_VEL = 1;
 
@@ -169,10 +165,10 @@ class Entity {
 
         bool collision;
 
-		// the unique ID for every entity
-		int ID, BRO;
-		static int current_ID;
-		static int counter_BRO;
+	// the unique ID for every entity
+	int ID, BRO;
+	static int current_ID;
+	static int counter_BRO;
 
         int             X, X1, X2;
         int             Y, Y1, Y2;
@@ -191,29 +187,28 @@ class Entity {
         int             Width, W2;		// for SDL2 rendering: Width for destination SDL_Rect, W2 for source SDL_Rect
         int             Height, H2;		// as like as Width/W2 properties
 
-        int 			selected;
+        int 		selected;
 
         int             counter;
-		int 			Laser_time;
-		int				targetID;
+	int		Laser_time;
+	int		targetID;
 
         int             PlayerScore;
         int             playerHonor;
         int             medalHonor;
         int             medalReward;
-		int 			stamina;
+	int		stamina;
 
         //-------------------------
         //SDL2 collision properties
         int mPosX, mPosY;
         //The velocity of the dot
-		int mVelX, mVelY;
+	int mVelX, mVelY;
         
-		// a container for every entity pixel to detect a collision 
-		std::vector<SDL_Rect> mColliders;
-	    int r;
-        
-		
+	// a container for every entity pixel to detect a collision 
+	std::vector<SDL_Rect> mColliders;
+	int r;
+        	
         int         AnimState;
 
         int         Sound_explSX, Sound_PlayerFire, Sound_PlayerXPL;
@@ -226,7 +221,7 @@ class Entity {
         int         Type, Size, Flags, Move;
         eMove       motion;
 		
-		int 		Pattern;
+	int	    Pattern;
         int         EnemyFireMode;
 
         bool        Dead = false;
@@ -244,11 +239,10 @@ class Entity {
         
     
 
-		Entity();
+	Entity();
 		
-		virtual ~CEntity();
+	virtual ~CEntity();
 
-	
         virtual bool OnLoad(char* File, int Width, int Height, int MaxFrames);
 
         virtual bool OnLoad(char* File, int Width, int Height);
@@ -267,11 +261,11 @@ class Entity {
 
         virtual void OnAnimate();
 
-		virtual bool OnCollision(CEntity* Entity);		// collision detected, here we select what to do
+	virtual bool OnCollision(CEntity* Entity);		// collision detected, here we select what to do
 
-		virtual void CollisionLoop();		// check entity collision
+	virtual void CollisionLoop();		// check entity collision
 
-		std::vector<SDL_Rect>& getColliders();
+	std::vector<SDL_Rect>& getColliders();
         virtual void shiftColliders();
 
         virtual void OnFire();
@@ -281,35 +275,35 @@ class Entity {
 
         virtual void IA();		// enetity enemie's "mind"
 
-		static int GetX();
-		static int GetY();
+	static int GetX();
+	static int GetY();
 
-		static CEntity* GetEntity(int temp_ID);
+	static CEntity* GetEntity(int temp_ID);
 
-		int GetEntityMoving();
+	int GetEntityMoving();
 
-		virtual int		EnemyGetX();
-		virtual int		EnemyGetY();
+	virtual int		EnemyGetX();
+	virtual int		EnemyGetY();
 
-		virtual float	PlayerGetX();
-		virtual float 	PlayerGetY();
+	virtual float	PlayerGetX();
+	virtual float 	PlayerGetY();
 
-		bool    Jump();
+	bool    Jump();
 
-		bool    Crash();
+	bool    Crash();
 
-		void    ScoreInc();
+	void    ScoreInc();
 
     public:
 
         void    OnMove(float MoveX, float MoveY);
 
         void    StopMove();
-		void    StopMoveX();
-		void    StopMoveY();
+	void    StopMoveX();
+	void    StopMoveY();
 
 
-		/* old func for pixel collision detection (not used anymore) 
+	/* old funcs for pixel collision detection (not used anymore) 
         SDL_Rect GetBounds();
 
         SDL_Rect NormalizeBounds(const SDL_Rect& rect);
@@ -324,7 +318,7 @@ class Entity {
         bool CheckCollision(std::vector<SDL_Rect>& a, std::vector<SDL_Rect>& b);
         void CheckCollision(CEntity* Entity);
         bool GetAlphaXY(CEntity* Entity, int x, int y);
-		*/
+	*/
 
 };
 
@@ -332,15 +326,18 @@ class Entity {
 class CEntityCol {
 
     public:
-        static std::vector<CEntityCol>  EntityColList;
+        
+	static std::vector<CEntityCol>  EntityColList;
 
     public:
-        CEntity *EntityA;
+        
+	CEntity *EntityA;
         CEntity *EntityB;
 
 
     public:
-        CEntityCol();
+        
+	CEntityCol();
 };
 
 
